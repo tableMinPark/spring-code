d87901a81164e9df08e51d3ceb0b3e7e7ac3f8ed
ParkSangmin <tablemin_park@daum.net>
2022-01-07 14:07:08 +0900

    Update ch2
    2-20 ~ 21 GetMapping, PostMapping

--- ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
+++ ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
@@ -1,17 +1,41 @@
 package com.fastcampus.ch2;
 
+import java.net.URLEncoder;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RegisterController {
-	@RequestMapping("/register/add")
+	@RequestMapping(value="/register/add", method={RequestMethod.GET, RequestMethod.POST})
 	public String register() {
 		return "registerForm";	// WEB-INF/views/registerForm.jsp	
 	}
+// servlet-context.xml 에 view-controller 등록해서 필요없어짐.
 	
-	@RequestMapping("/register/save")
-	public String save() {
+//	@RequestMapping(value="/register/save", method=RequestMethod.POST)
+	@PostMapping("/register/save")	// 4.3 부터
+	public String save(User user, Model m) throws Exception {
+		// 1. 유효성 검사
+		if (!isValid(user)) {
+			String msg = URLEncoder.encode("ID를 잘못 입력하셨습니다.", "utf-8");
+			
+			m.addAttribute("msg", msg);
+			return "forward:/register/add";
+//			return "redirect:/register/add";
+			
+//			return "redirect:/register/add?msg="+msg; // URL 재작성 (rewriting)
+		}
+		
+		// 2. DB에 신규회원 정보를 저장
 		return "registerInfo";
+	}
+
+	private boolean isValid(User user) {
+		return false;
 	}
 }
--- ch2/src/main/webapp/WEB-INF/views/registerInfo.jsp
+++ ch2/src/main/webapp/WEB-INF/views/registerInfo.jsp
@@ -7,6 +7,13 @@
 <title>Insert title here</title>
 </head>
 <body>
+<h1>id=${user.id}</h1>
+<h1>pwd=${user.pwd}</h1>
+<h1>name=${user.name}</h1>
+<h1>email=${user.email}</h1>
+<h1>birth=${user.birth}</h1>
+<h1>sns=${user.sns}</h1>
+<%-- 
 <h1>id=${param.id}</h1>
 <h1>pwd=${param.pwd}</h1>
 <h1>name=${param.name}</h1>
@@ -16,6 +23,7 @@
 <h1>sns=${paramValues.sns[0]}</h1>
 <h1>sns=${paramValues.sns[1]}</h1>
 <h1>sns=${paramValues.sns[2]}</h1>
-<!-- paramValues : 같은 key인 여러개의 value를 받을 때 사욯한다. (sns라는 key를 가진 3개의 value를 배열을 통해 받아 출력) : EL에서의 사용법 -->
+--%>
+<!-- userValues : 같은 key인 여러개의 value를 받을 때 사욯한다. (sns라는 key를 가진 3개의 value를 배열을 통해 받아 출력) : EL에서의 사용법 -->
 </body>
 </html>
