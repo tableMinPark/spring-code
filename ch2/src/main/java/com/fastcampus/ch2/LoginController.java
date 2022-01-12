7d9c13d509d9f4cd2a5b45e4e6490ee4cce8f6dd
ParkSangmin <tablemin_park@daum.net>
2022-01-13 02:15:20 +0900

    Update ch2
    2-23 ~ 24 Session

--- ch2/src/main/java/com/fastcampus/ch2/LoginController.java
+++ ch2/src/main/java/com/fastcampus/ch2/LoginController.java
@@ -3,9 +3,12 @@ package com.fastcampus.ch2;
 import java.net.URLEncoder;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,19 +21,29 @@ public class LoginController {
 		return "loginForm";
 	}
 	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		// 1. 세션을 종료
+		session.invalidate();
+		// 2. 홈으로 이동
+		return "redirect:/";
+	}
+	
 	@PostMapping("/login")
-	public String login(String id, String pwd, boolean rememberId, HttpServletResponse response) throws Exception {
-		System.out.println("id : " + id);
-		System.out.println("pwd : " + pwd);
-		System.out.println("rememberId : " + rememberId);	// checkbox의 타입을 boolean, String(on/ off) 두가지로 사용할 수 있다.
-		
+	public String login(String id, String pwd, boolean rememberId, String toURL, 
+			HttpServletRequest request , HttpServletResponse response,
+			@CookieValue("id") String cookieId) throws Exception {
+
 		// 1. id와 pwd를 확인
 		if (!loginCheck(id, pwd)) {
 			String msg = URLEncoder.encode("id 또는 pwd가 일지하지 않습니다.", "utf-8");
 		// 2-1. id와 pwd가 일치하지 않으면, loginForm으로 이동 (redirect)
 			return "redirect:/login/login?msg="+msg;
 		}
-		// 2-2. id와 pwd가 일치하면, 홈으로이동
+		// 2-2. id와 pwd가 일치하면, 세션 객체에 id를 저장
+		HttpSession session = request.getSession();
+		session.setAttribute("id", id);
+		
 		if (rememberId) {
 			// 3-1. 쿠키를 생성
 			Cookie cookie = new Cookie("id", id);
@@ -46,7 +59,9 @@ public class LoginController {
 		}		
 		
 		// 3-3. 홈으로 이동
-		return "redirect:/";
+		toURL = toURL == null || toURL.equals("") ? "/" : toURL;
+		
+		return "redirect:"+toURL;
 	}
 
 	private boolean loginCheck(String id, String pwd) {
